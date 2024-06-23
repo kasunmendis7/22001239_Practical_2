8683f4c7d2d7e067768a6ab2f6f75c19c21bb3b2
kasunmendis7 <tkomendis@gmail.com>
2024-06-23 06:47:36 +0530

    Question 4

---
+++ .metals/.reports/metals/2024-06-23/r_scala-toplevel-mtags_(22001239_Practical_2_f4dd477a3a)_06-37-25-664.md
@@ -0,0 +1,23 @@
+error id: file:///mnt/a66ad743-1601-4348-99ee-1e8f5b440f29/UCSC/SCS%202204%20-%20Functional%20Programming/Practicals/22001239_Practical_2/Question%204/Question4.scala:[80..83) in Input.VirtualFile("file:///mnt/a66ad743-1601-4348-99ee-1e8f5b440f29/UCSC/SCS%202204%20-%20Functional%20Programming/Practicals/22001239_Practical_2/Question%204/Question4.scala", "object Question4{
+    def main(args: Array[String]):Unit{
+
+    }
+
+    def 
+
+    def priceIncrement(price: Double, attendance: Int):Double={
+        (price+5) * (attendance-20)
+    }
+
+    def priceDecrement(price: Double, attendance: Int):Double={
+        (price-5) * (attendance+20)
+    }
+
+}")
+file:///mnt/a66ad743-1601-4348-99ee-1e8f5b440f29/UCSC/SCS%202204%20-%20Functional%20Programming/Practicals/22001239_Practical_2/Question%204/Question4.scala
+file:///mnt/a66ad743-1601-4348-99ee-1e8f5b440f29/UCSC/SCS%202204%20-%20Functional%20Programming/Practicals/22001239_Practical_2/Question%204/Question4.scala:8: error: expected identifier; obtained def
+    def priceIncrement(price: Double, attendance: Int):Double={
+    ^
+#### Short summary: 
+
+expected identifier; obtained def
---
+++ Question 4/Question4.scala
@@ -0,0 +1,37 @@
+/* 
+4. Imagine the owner of a movie theater who has complete freedom in setting 
+ticket prices. The more he charges, the fewer the people who can afford tickets.
+In a recent experiment the owner determined a precise relationship between the
+price of a ticket and average attendance. 
+At a price of Rs 15.00 per ticket, 120 people attend a performance. Decreasing 
+the price by 5 Rupees increases attendance by 20 and increasing the price by 5 
+Rupees decreases attendance by 20. 
+Unfortunately, the increased attendance also comes at an increased cost. Every 
+performance costs the owner Rs.500. Each attendee costs another 3 Rupees. 
+The owner would like to know the exact relationship between profit and ticket 
+price so that he can determine the price at which he can make the highest profit. 
+Implement a functional program to find out the best ticket price */
+object Question4 {
+  def main(args: Array[String]): Unit = {
+    println("Current profit gained from the movie theater = " + profit(15, 120))
+    println(
+      "Profit gained from the movie theater if price is increased = " + priceIncrement(15, 120)
+    )
+    println(
+      "Profit gained from the movie theater if price is decreased = " + priceDecrement(15, 120)
+    )
+  }
+
+  def profit(price: Double, attendance: Int): Double = {
+    (price * attendance) - 500 - (attendance*3)
+  }
+
+  def priceIncrement(price: Double, attendance: Int): Double = {
+    (price + 5) * (attendance - 20) - 500 - ((attendance-20)*3)
+  }
+
+  def priceDecrement(price: Double, attendance: Int): Double = {
+    (price - 5) * (attendance + 20) - 500 - ((attendance+20)*3)
+  }
+
+}
